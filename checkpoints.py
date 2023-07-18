acb13815cf5326d38d664f9223966c83f2d4d2d6
Aquapup <134471692+Aquapup@users.noreply.github.com>
2023-07-17 22:06:38 -0400

    Add files via upload

--- checkpoints.py
+++ checkpoints.py
@@ -38,9 +38,9 @@ class Solution:
                  for i in range (0, len(arr)):
                       for j in range(0, len(arr - 1)):
                         if arr[j] > arr[j + 1]:
-                           temp = arr[j]
-                           arr[j] = arr[j + 1]
-                           arr[j + 1] = temp
+                            temp = arr[j]
+                            arr[j] = arr[j + 1]
+                            arr[j + 1] = temp
                         return arr
     
             checkpoints = bubblesort(checkpoints)
